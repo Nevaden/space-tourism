c232c3b60b7d64afaed7336f3b22026172be598c
Nevaden <aaron.k.dodson@gmail.com>
2022-10-11 17:21:45 -0400

    no navigation if already on page setup for mobile/desktop.

--- src/app/components/shared/nav/nav.component.ts
+++ src/app/components/shared/nav/nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -6,11 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav.component.css']
 })
 export class NavComponent implements OnInit {
+  data: any;
+  navDestination: any;
+  destination: any;
+  navCrew: any;
+  navTechnology: any;
+  parentPath: any; 
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.routeSub();
   }
+  routeSub(){
+  
+    this.route.url.subscribe(m => this.parentPath = m[0].path)
+
+    // if(this.parentPath == "destination"){
+    //   if(this.destination==null ||this.destination==undefined ){
+    //     console.log(this.destination,"wma I really null?")
+    //     this.data = sessionStorage.getItem('destination')
+    //     this.destination = JSON.parse(this.data)
+    //     console.log(this.destination)
+    //     this.navDestination = ['/destination', Object.entries(this.destination)[0][0]]
+    //     console.log(this.navDestination,"on des, data null")
+          
+    //   } 
+    //   else{
+    //     console.log("else data exists")
+    //     this.navDestination = ['/destination',Object.entries(this.destination)[0][0]];
+    //   } 
+
+    // } else{
+    //   console.log("not on same page")
+    //   this.navDestination = ['/destination'];
+    // }
+    
+
+  }
+
   toggleNavModal(){
     const modal = document.getElementById('navModal')
     const backDrop = document.getElementById('backDrop')
